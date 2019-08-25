9d1d1b344418de8b37f3ac4343cf216180cb9083
Artur Ciemiorek <artur.ciemiorek@gmail.com>
2019-08-25 14:57:22 +0200

    create add result object

---
+++ src/main/java/com/example/DatabaseApp.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import java.sql.*;
+
+
+public class DatabaseApp {
+
+    private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/notesdb";
+    private static final String DATABASE_USER = "postgres";
+    private static final String DATABASE_PASS = "postgres";
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+         Class.forName("org.postgresql.Driver");
+
+        Connection connection = DriverManager.getConnection(JDBC_URL, DATABASE_USER, DATABASE_PASS);
+        Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM notes;");
+
+
+
+        statement.close();
+        connection.close();
+
+    }
+}
